273c0e8555ff1acdf244702587bdc809f49118a5
myjhye <t01087633799@gmail.com>
2024-01-19 04:22:30 +0900

    마케팅 페이지

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,19 @@
+import { Footer } from "./_components/footer";
+import { Navbar } from "./_components/navbar";
+
+const MarketingLayout = ({children}: {children: React.ReactNode;}) => {
+    return (
+        <div className="h-full bg-slate-100">
+            {/* navbar */}
+            <Navbar />
+            {/* content */}
+            <main className="pt-40 pb-20 bg-slate-100">
+                {children}
+            </main>
+            {/* footer */}
+            <Footer />
+        </div>
+    );
+};
+
+export default MarketingLayout;
