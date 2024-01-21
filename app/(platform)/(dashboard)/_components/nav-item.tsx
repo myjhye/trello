2be6c27552a80105026ffe9dda79547e4880bebe
myjhye <t01087633799@gmail.com>
2024-01-21 17:06:29 +0900

    사이드바 로딩 시 표시되는 skeleton

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -109,3 +109,14 @@ export const NavItem = ({isExpanded, isActive, organization, onExpand}: NavItemP
         </AccordionItem>
       );
     };
+
+    NavItem.Skeleton = function SkeletonNavItem() {
+      return (
+        <div className="flex items-center gap-x-2">
+          <div className="w-10 h-10 relative shrink-0">
+            <Skeleton className="h-full w-full absolute" />
+          </div>
+          <Skeleton className="h-10 w-full" />
+        </div>
+      )
+    }
