7528f81e3a188a96c03e5a6befbfc1807b895f30
myjhye <t01087633799@gmail.com>
2024-01-27 11:07:55 +0900

    board popover form

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,4 +1,6 @@
-import { User2 } from "lucide-react";
+import FormPopover from "@/components/form/form-popover";
+import Hint from "@/components/hint";
+import { HelpCircle, User2 } from "lucide-react";
 
 export default function BoardList() {
     return (
@@ -8,15 +10,28 @@ export default function BoardList() {
                 Your boards
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-                <div 
-                    role="button"
-                    className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
-                >
-                    <p>새 보드 생성</p>
-                    <span className="text-sm">
-                        5개 남음
-                    </span>
-                </div>
+                {/* 팝오버 */}
+                <FormPopover sideOffset={10} side="right">
+                    <div 
+                        role="button"
+                        className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+                    >
+                        <p>새 보드 생성</p>
+                        <span className="text-sm">
+                            5개 남음
+                        </span>
+                        <Hint
+                            sideOffset={40}
+                            description={`
+                                무료 워크스페이스에서는 최대 5개의 보드 생성 가능. 무제한 보드를 사용하려면 업그레이드.
+                            `}
+                        >
+                            <HelpCircle 
+                                className="absolute bottom-2 right-2 h-[14px] w-[14px]"
+                            />
+                        </Hint>
+                    </div>
+                </FormPopover>
             </div>
         </div>
     )
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -6,8 +6,11 @@ const OrganizationIdPage = async () => {
 
     return (
         <div className="flex flex-col space-y-4">
+            {/* 인포 */}
             <Info />
+            {/* 분리선 */}
             <Separator className="my-4" />
+            {/* 보드 목록 */}
             <div className="px-2 md:px-4">
                 <BoardList />
             </div>
