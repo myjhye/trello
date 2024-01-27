6e0f36616ff79e933fa4d9a3ccccaa480c5f5699
myjhye <t01087633799@gmail.com>
2024-01-28 00:37:57 +0900

    image 스키마 유효성 추가

--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -1,10 +1,16 @@
 import { z } from "zod";
 
 export const CreateBoard = z.object({
+    
     title: z.string({
         required_error: "제목 입력 필요",
         invalid_type_error: "제목 입력 필요",
     }).min(3, {
         message: "제목이 너무 짧습니다"
+    }),
+
+    image: z.string({
+        required_error: "이미지 입력 필요",
+        invalid_type_error: "이미지 입력 필요",
     }),
 });
