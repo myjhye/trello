b1daba8c63bd68e6eb89acd27cf3a28f82def12a
myjhye <t01087633799@gmail.com>
2024-01-28 19:38:06 +0900

    board list 보드 목록

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,15 +1,53 @@
 import FormPopover from "@/components/form/form-popover";
 import Hint from "@/components/hint";
+import { Skeleton } from "@/components/ui/skeleton";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 import { HelpCircle, User2 } from "lucide-react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function BoardList() {
+export default async function BoardList() {
+
+    // 사용자의 orgId 가져오기
+    const { orgId } = auth();
+
+    // orgId가 없으면 /select-org으로 리다이렉트
+    if (!orgId) {
+        return redirect("/select-org");
+    }
+
+    // 사용자의 orgId에 해당하는 보드 목록 가져오기
+    const boards = await db.board.findMany({
+        where: {
+            orgId
+        },
+        orderBy: {
+            createdAt: "desc"
+        }
+    });
+
     return (
         <div className="space-y-4">
-            <div className="flex items-center font-semibold text-lg text-neutral-700">
-                <User2 className="h-6 w-6 mr-2" />
+          <div className="flex items-center font-semibold text-lg text-neutral-700">
+            <User2 className="h-6 w-6 mr-2" />
                 Your boards
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+                {/* 보드목록 */}
+                {boards.map((board) => (
+                <Link
+                    key={board.id}
+                    href={`/board/${board.id}`}
+                    className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
+                    style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+                >
+                    <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+                    <p className="relative font-semibold text-white">
+                        {board.title}
+                    </p>
+                </Link>
+                ))}
                 {/* 팝오버 */}
                 <FormPopover sideOffset={10} side="right">
                     <div 
@@ -35,4 +73,20 @@ export default function BoardList() {
             </div>
         </div>
     )
-}
+};
+
+// 스켈레톤
+BoardList.Skeleton = function SkeletonBoardList() {
+    return (
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+        </div>
+    );
+};
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,18 +1,21 @@
 import { Separator } from "@/components/ui/separator";
 import Info from "./_components/info";
 import BoardList from "./_components/board-list";
+import { Suspense } from "react";
 
 const OrganizationIdPage = async () => {
 
     return (
-        <div className="flex flex-col space-y-4">
+        <div className="w-full mb-20">
             {/* 인포 */}
             <Info />
             {/* 분리선 */}
             <Separator className="my-4" />
             {/* 보드 목록 */}
             <div className="px-2 md:px-4">
-                <BoardList />
+                <Suspense fallback={<BoardList.Skeleton />}>
+                    <BoardList />
+                </Suspense>
             </div>
         </div>
     )
