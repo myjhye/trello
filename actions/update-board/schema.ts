3bd3e7fa942ec7c69b880f89a8da3f5aab6ab9f2
myjhye <t01087633799@gmail.com>
2024-01-30 20:41:37 +0900

    서버 - 수정

---
+++ actions/update-board/schema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const UpdateBoard = z.object({
+
+    title: z.string({
+        required_error: "제목 입력 필요",
+        invalid_type_error: "제목 입력 필요",
+    }).min(3, {
+        message: "제목이 너무 짧습니다"
+    }),
+
+    id: z.string(),
+});
