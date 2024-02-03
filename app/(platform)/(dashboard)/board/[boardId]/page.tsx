4133ca6dd371c2a60d4460dbc39198626d484b71
myjhye <t01087633799@gmail.com>
2024-02-03 11:32:52 +0900

    주석

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -17,20 +17,26 @@ export default async function BoardIdPage({params}: BoardIdPageProps) {
         redirect("/select-org");
     }
 
+    // 보드의 모든 리스트와 각 리스트에 포함된 카드 조회
     const lists = await db.list.findMany({
         where: {
+            // 해당 boardId에 속한 리스트 조회
             boardId: params.boardId,
+            // 해당 orgId에 속한 보드의 리스트 조회
             board: {
                 orgId,
             },
         },
+        // 각 리스트에 속한 카드도 결과에 포함
         include: {
             cards: {
+                // 카드 오름차순 정렬
                 orderBy: {
                     order: "asc",
                 },
             },
         },
+        // 리스트 오름차순 정렬
         orderBy: {
             order: "asc",
         },
