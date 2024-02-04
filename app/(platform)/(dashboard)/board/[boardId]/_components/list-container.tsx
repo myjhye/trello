79afd33b5ab3113fa8b0dcc2cc3705d2a3fb9ee1
myjhye <t01087633799@gmail.com>
2024-02-04 09:17:44 +0900

    드래그앤드롭 -> 원위치로 복귀

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -4,6 +4,7 @@ import { ListWithCards } from "@/types";
 import ListForm from "./list-form";
 import { useEffect, useState } from "react";
 import ListItem from "./list-item";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 
 interface ListContainerProps {
     boardId: string;
@@ -21,20 +22,35 @@ export default function ListContainer({boardId, data}: ListContainerProps) {
     }, [data]);
 
     return (
-        <ol className="flex gap-x-3 h-full">
-            {/* 보드 목록 */}
-            {orderedData.map((list, index) => {
-              return (
-                <ListItem
-                  key={list.id}
-                  index={index}
-                  data={list}
-                />
-              )
-            })}
-            {/* 보드 입력 */}
-            <ListForm />
-            <div className="flex-shrink-0 w-1" />
-        </ol>
+      <DragDropContext onDragEnd={() => {}}>
+          <Droppable 
+            droppableId="lists" 
+            type="list" 
+            direction="horizontal"
+          >
+            {(provided) => (
+              <ol
+                { ...provided.droppableProps }
+                ref={provided.innerRef} 
+                className="flex gap-x-3 h-full"
+              >
+                  {/* 보드 목록 */}
+                  {orderedData.map((list, index) => {
+                    return (
+                      <ListItem
+                        key={list.id}
+                        index={index}
+                        data={list}
+                      />
+                    )
+                  })}
+                  {provided.placeholder}
+                  {/* 보드 입력 */}
+                  <ListForm />
+                  <div className="flex-shrink-0 w-1" />
+              </ol>
+            )}
+          </Droppable>
+        </DragDropContext>
     )
 }
