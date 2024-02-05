ecc92498c0785c8c01581f2ee973cc8f20241498
myjhye <t01087633799@gmail.com>
2024-02-06 05:31:53 +0900

    description 업데이트

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -12,11 +12,12 @@ export const UpdateCard = z.object({
             message: "내용이 너무 짧습니다"
         }),
     ),
-    title: z.string({
+    title: z.optional( 
+        z.string({
         required_error: "제목 입력 필요",
         invalid_type_error: "제목 입력 필요",
     }).min(3, {
         message: "제목이 너무 짧습니다"
-    }),
+    })),
     
 });
