b154228e1b09a2a622ca3d776132406fcc60197d
myjhye <t01087633799@gmail.com>
2024-02-07 05:59:47 +0900

    카드 복사, 삭제 - 프론트

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -7,6 +7,7 @@ import { CardWithList } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import Header from "./header";
 import Description from "./description";
+import Actions from "./actions";
 
 export const CardModal = () => {
     const id = useCardModal((state) => state.id);
@@ -37,6 +38,10 @@ export const CardModal = () => {
                             }
                         </div>
                     </div>
+                    {!cardData
+                        ? <Actions.Skeleton />
+                        : <Actions data={cardData} />
+                    }
                 </div>
             </DialogContent>
         </Dialog>
