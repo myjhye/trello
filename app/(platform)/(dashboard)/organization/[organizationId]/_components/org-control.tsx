b8753996e520ab60c9b38d320d8b059599c6c4e8
myjhye <t01087633799@gmail.com>
2024-02-13 19:41:31 +0900

    주석 -> orgControl

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -6,18 +6,23 @@ import { useEffect } from "react";
 
 export const OrgControl = () => {
 
+    // url 파라미터
     const params = useParams();
+    // 사용자가 선택한 조직 활성화(접속)
     const { setActive } = useOrganizationList();
 
     useEffect(() => {
         if (!setActive) {
             return;
         }
+        // 사용자가 조직 선택 시 해당 조직의 id가 url의 파라미터로 설정되고, 이후 setActive를 호출해 해당 조직 활성화
         setActive({
             organization: params.organizationId as string,
         });
 
+    // 사용자가 조직 선택 시, url 파라미터 변경 시
     }, [setActive, params.organizationId]);
 
+    // 현재 컴포넌트는 setActive로 조직을 활성화하지만 실제 화면에는 아무것도 표시하지 않음
     return null;
 }
